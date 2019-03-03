1b23277353d36e38852300d23ee1de449f83b808
Sofmic <d.kasp.poczta@gmail.com>
2019-03-04 00:22:22 +0100

    cd

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 const pole = document.getElementById('blok');
 const d = pole.getContext('2d');
 
-d.beginPath();
-d.moveTo(0,200);
-d.lineTo(10,190);
-d.lineTo(20,200);
-d.lineTo(0,200);
-d.stroke();
+const niebo = new Image();
+const bohater = new Image();
+const droga = new Image();
+
+function tlo(){
+
+	niebo.addEventListener("load",function(){	d.drawImage(niebo,0,0)	}); niebo.src = "img/niebo.png";
+	droga.addEventListener('load',function(){	d.drawImage(droga,0,340);	}); droga.src = "img/droga.png"
+
+}
+
+function postacDwa(){
+
+	bohater.src = "img/postac2.png";
+	tlo();
+	d.drawImage(bohater,0,320);
+}
+
+function postacJeden(){
+
+	bohater.src = "img/postac.png";
+	tlo();
+	d.drawImage(bohater,0,320);
+}
+
+tlo();
+
+bohater.addEventListener("load",function(){	
+	d.drawImage(bohater,0,320)	
+})
+
+bohater.src = "img/postac.png";
+
+
+document.addEventListener("keydown",function(k){  
+	if( k.key === "ArrowUp" ){	 
+		if(bohater.src === "img/postac2.png")
+		postacJeden();	
+	else
+		postacDwa();
+	}
+})
+
+
+
+
+	
+	
+
