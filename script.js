bb8b217b59c97ade259dce4bffb128f07bcdd651
Sofmic <d.kasp.poczta@gmail.com>
2019-03-05 17:53:50 +0100

    asd

--- script.js
+++ script.js
@@ -4,46 +4,55 @@ const d = pole.getContext('2d');
 const niebo = new Image();
 const bohater = new Image();
 const droga = new Image();
+const przeszkoda = new Image();
 
-function tlo(){
-
-	niebo.addEventListener("load",function(){	d.drawImage(niebo,0,0)	}); niebo.src = "img/niebo.png";
-	droga.addEventListener('load',function(){	d.drawImage(droga,0,340);	}); droga.src = "img/droga.png"
-
-}
-
-function postacDwa(){
-
-	bohater.src = "img/postac2.png";
-	tlo();
-	d.drawImage(bohater,0,320);
-}
-
-function postacJeden(){
+function postac(y){
+	bohater.addEventListener("load",function(){ 
+		d.drawImage(bohater,0,y) 
+	}); 
 
 	bohater.src = "img/postac.png";
-	tlo();
-	d.drawImage(bohater,0,320);
 }
 
-tlo();
+function sciezka(){
+	droga.addEventListener('load',function(){	
+		d.drawImage(droga,0,340); postac(320)
+	}); 
 
-bohater.addEventListener("load",function(){	
-	d.drawImage(bohater,0,320)	
-})
-
-bohater.src = "img/postac.png";
+	droga.src = "img/droga.png";
+}
 
 
-document.addEventListener("keydown",function(k){  
-	if( k.key === "ArrowUp" ){	 
-		if(bohater.src === "img/postac2.png")
-		postacJeden();	
-	else
-		postacDwa();
-	}
-})
+function tlo(){
+	niebo.addEventListener("load",function(){	
+		d.drawImage(niebo,0,0); 
+		sciezka();	
+	}); 
 
+	niebo.src = "img/niebo.png";
+}
+
+tlo()
+
+let idrequest;
+
+function petla(){
+
+	//kod z przeszkodami
+
+}
+
+function start(){
+	
+	idrequest = requestAnimationFrame(petla);
+
+}
+
+function stop(){
+	
+	cancelAnimationFrame(idrequest);
+
+}
 
 
 
